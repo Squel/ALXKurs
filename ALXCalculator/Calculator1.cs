6bdd0b83fd61855437bdc64a192ccc37cc61063f
Michał Buczyński <michal.buczynski@wum.edu.pl>
2022-10-30 13:30:35 +0100

    Wersja 2.1

--- ALXCalculator/Calculator1.cs
+++ ALXCalculator/Calculator1.cs
@@ -10,47 +10,51 @@ namespace ALXCalculator
     {
         public void Run1()
         {
-            Console.WriteLine("Running calculator.....");
+                    Console.WriteLine("Running calculator.....");
+                    Console.Write("Podaj pierwsza liczbe: ");
+                    var x = double.Parse(Console.ReadLine());
+                    Console.Write("Podaj druga liczbe: ");
+                    var y = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Wybierz dzialanie: ");
+                    Console.WriteLine("1. Dodawanie ");
+                    Console.WriteLine("2. Mnozenie ");
+                    Console.WriteLine("3. Odejmowanie ");
+                    Console.WriteLine("4. Dzielenie ");
+                    var z = Int32.Parse(Console.ReadLine());
+                    if (z == 1) {
+                        Console.WriteLine($"{x} + {y} = {Add(x, y)}");
+                    }
+                    if (z == 2) {
+                        Console.WriteLine($"{x} * {y} = {Multiply(x, y)}");
+                    }
+                    if (z == 3)
+                    {
+                        Console.WriteLine($"{x} - {y} = {Substract(x, y)}");
+                    }
+                    if (z == 4)
+                    {
+                        Console.WriteLine($"{x} / {y} = {Divide(x, y)}");
+                    }
+                }
+                private double Add(double x, double y)
+                {
+                    return x + y;
+                }
+
+                private double Multiply(double x, double y)
+                {
+                    return x * y;
+                }
+
+                private double Substract(double x, double y)
+                {
+                    return x - y;
+                }
+                private double Divide(double x, double y)
+                {
+                    return x / y;
+                }
             
-            
-            Console.Write("Podaj pierwsza liczbe: ");
-            var x = double.Parse(Console.ReadLine());
-            Console.Write("Podaj druga liczbe: ");
-            var y = double.Parse(Console.ReadLine());
-            Console.WriteLine("Wybierz dzialanie: ");
-            Console.WriteLine("1. Dodawanie ");
-            Console.WriteLine("2. Mnozenie ");
-            Console.WriteLine("3. Odejmowanie ");
-            Console.WriteLine("4. dzielenie ");
-            var z = Int32.Parse (Console.ReadLine());
-            if  {
-                Console.WriteLine($"{x} + {y} = {Add(x, y)}");
-            }
-            if  {
-
-            }
-                   
-            Console.WriteLine($"{x} * {y} = {Multiply(x,y)}");        
-            Console.WriteLine($"{x} - {y} = {Substract(x,y)}");        
-            Console.WriteLine($"{x} / {y} = {Divide(x,y)}");        
-        }
-        private double Add(double x, double y)
-        {
-            return x + y;
-        }
-
-        private double Multiply(double x, double y)
-        {
-            return x * y;
-        }
-
-        private double Substract (double x, double y)
-        {
-            return x - y;
-        }
-        private double Divide(double x, double y)
-        {
-            return x / y;
-        }
+        
     }
 }
