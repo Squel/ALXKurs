d842c3795d9f1c92b97661ed0baaa87db72634a1
michał.buczyński <micha.buczyski@wum.edu.pl>
2022-11-03 15:02:39 +0100

    2.5

--- ALXCourseHomework/Homework5Test.cs
+++ ALXCourseHomework/Homework5Test.cs
@@ -18,7 +18,7 @@ namespace ALXCourseHomework
             Console.WriteLine("Kwota drugiego towaru: ");
             var y = double.Parse(Console.ReadLine());
             var z = (x + y);
-            Console.WriteLine($"Suma zakupionych towarów: {x + y}");
+            Console.WriteLine($"Suma zakupionych towarów: {z}");
             
             
             switch (z)
@@ -26,12 +26,14 @@ namespace ALXCourseHomework
                 case > 100:
                     Console.WriteLine("Udzielono 15% rabatu");
                     z = (z * (1 - 0.15));
-                    Console.WriteLine($"Kwota do zapłaty: {z}");
+                    Double dz = Math.Round((Double)z, 2);
+                    Console.WriteLine($"Kwota do zapłaty: {dz}");
                     break;
                 case <= 100 and >60:
                     Console.WriteLine("Udzielono 5% rabatu");
                     z = (z * (1 - 0.05));
-                    Console.WriteLine($"Kwota do zapłaty: {z}");
+                    Double cz = Math.Round((Double)z, 2);
+                    Console.WriteLine($"Kwota do zapłaty: {cz}");
                     break;
                default:
                     Console.WriteLine("Kwota nie podlega rabatowi");
