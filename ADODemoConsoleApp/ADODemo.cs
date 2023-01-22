e6544c14bd0eac0a790543795d9c69858e323a24
Michał Buczyński <michal.buczynski@wum.edu.pl>
2023-01-22 14:51:35 +0100

    Wersja 3.4

---
+++ ADODemoConsoleApp/ADODemo.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.SqlClient;
+
+namespace ADODemoConsoleApp
+{
+    public class ADODemo
+    {
+
+        private string _connectionString;
+
+        public ADODemo(string connectionString)
+        {
+            _connectionString= connectionString;
+        }
+
+        public void RunSelectAllFromEmployees()
+        {
+            var queryString = "Select * from employees";
+
+            using var connection = new SqlConnection(_connectionString);
+            var sqlCommand = new SqlCommand(queryString, connection);
+            try
+            {
+                connection.Open();
+                var sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine($"{sqlDataReader[0]},{sqlDataReader[1]},{sqlDataReader[2]},{sqlDataReader[3]}");
+                }
+                sqlDataReader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public void RunSelectedDefinedColumnSet(string column1, string column2, string column3)
+        {
+           
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var queryString = $"select {@column1}, {@column2}, {@column3} from employees";
+                SqlCommand command = new SqlCommand(queryString, connection);
+               // command.Parameters.AddWithValue("@column1", column1);
+               // command.Parameters.AddWithValue("@column2", column2);
+           
+                try
+                {
+                    connection.Open();
+                    var sqlDataReader = command.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine($"{sqlDataReader[0]},{sqlDataReader[1]},{sqlDataReader[2]}");
+                    }
+                    sqlDataReader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+    }
+}
---
+++ ADODemoConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using ADODemoConsoleApp;
+
+Console.WriteLine("Hello, World!");
+var connectionString = "Data Source=DESKTOP-UOE11SG\\SQLEXPRESS; Initial Catalog=company; Integrated Security=True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+ADODemo demo = new ADODemo(connectionString);
+Console.WriteLine("Select all from table");
+demo.RunSelectAllFromEmployees();
+Console.WriteLine("---------------------");
+Console.WriteLine("Select columns");
+demo.RunSelectedDefinedColumnSet("first_name", "last_name", "email");
+Console.WriteLine("---------------------");
